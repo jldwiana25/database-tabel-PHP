d16ac87c92dd00f524950301af4640c587392e44
jldwiana25 <juliadwiana2521@gmail.com>
2018-10-27 21:40:15 +0700

    crud

--- index.php
+++ index.php
@@ -1,40 +1,42 @@
-<html>
-<head>
-  <title>Menampilkan data dari database ke dalam bentuk tabel</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+<?php 
+	include "koneksi.php";
+?>
+  <html>
+  <head>
+    <title>Menampilkan data dari database ke dalam bentuk tabel</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
 
-  <div class="container">
-  	<h2><center>DATA PESERTA</center></h2>
-      <a href="tambah.php">Tambah Data</a>
-  	<table class="tabel" align="center">
-  <tr>
-  	<th>id</th>
-  	<th>username</th>
-  	<th>password</th>
-  	<th>level</th>
-  	<th>fullname</th>
-  </tr>
-  </div>
-  <?php
-  // 1. Lakukan include koneksi.php untuk membuat koneksi
-  include('koneksi.php');
-  // 2. Buat perintah SQL untuk menampilkan data
-  $sql_tampil ="SELECT * FROM tabe_kelas";
-  // 3. Jalankan perintah diatas dengan fungsi mysqli_query
-  $peserta=mysqli_query($conn,$sql_tampil);
-  // 4. Lakukan fetch dengan result type MYSQL_ASSOC
-  while($baris_data=mysqli_fetch_array($peserta,MYSQLI_ASSOC)){
-  	echo'
-  	</tr>
-  		<td>'.$baris_data['id'].'</td>
-  		<td>'.$baris_data['username'].'</td>
-  		<td>'.$baris_data['password'].'</td>
-  		<td>'.$baris_data['level'].'</td>
-  		<td>'.$baris_data['fullname'].'</td>
-  	</tr>';
-  }
-  ?>
+    <div class="container">
+      <h2><center>DATA PESERTA</center></h2>
+        <a href="tambah.php">Tambah Data</a>
+      <table class="tabel" align="center">
+        <tr>
+          <th>id</th>
+          <th>username</th>
+          <th>password</th>
+          <th>level</th>
+          <th>fullname</th>
+        </tr>
+    </div>
+          <?php 
+            $qry = mysqli_query($conn,"SELECT * FROM tabe_kelas");
+            while($data = mysqli_fetch_array($qry)){
+          ?>
+          <tr>
+            <td><?php echo $data['id']; ?></td>
+            <td><?php echo $data['username']; ?></td>
+            <td><?php echo $data['password']; ?></td>
+            <td><?php echo $data['level']; ?></td>
+            <td><?php echo $data['fullname']; ?></td> 
+            <td>
+              <a onclick="return confirm('Yakin untuk menghapus ?')"
+              href="hapus.php?id=<?php echo $data['id']; ?>">Hapus</a>
+              <a href="update.php?id=<?php echo $data['id']; ?>">Edit</a>
+            </tr>
+        <?php
+        }
+        ?>
 </body>
 </html>
--- update.php
+++ update.php
@@ -1,10 +1,12 @@
 <?php
-	include "koneksi.php";
+    	include "koneksi.php";
+        $qry = mysqli_query($conn,"SELECT * FROM tabe_kelas");
+        $data = mysqli_fetch_array($qry);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tambah Data</title> 
+    <title>Edit Data</title>
 </head>
 <body>
     <form method="post">
@@ -12,33 +14,33 @@
             <tr><td>Id</td><td><input type="number" name="id"></td></tr>
             <tr><td>Username</td><td><input type="text" name="username"></td></tr>
             <tr><td>Password</td><td><input type="password" name="password"></td></tr>
-            <tr><td>Level</td><td> 
+            <tr><td>Level</td><td>
             <select name="level">
-                    <option value="User">User</option>
-                    <option value="Admin">Admin</option>
+				<option value="User">User</option>
+				<option value="Admin">Admin</option>
             </select>
             <tr><td>Fullname</td><td><input type="text" name="fullname"></td></tr>
-            <tr><td colspan="2"><input type="submit" name="simpan" value="Simpan"></td></tr>
+            <tr><td colspan="2"><input type="submit" name="edit" value="Edit"></td></tr>
         </table>
     </form>
     <?php 
- 		if (isset($_POST['simpan'])) {
+ 		if (isset($_POST['edit'])) {
  			$id = $_POST['id'];
  			$username = $_POST['username'];
  			$password = $_POST['password'];
  			$level = $_POST['level'];
  			$fullname = $_POST['fullname'];
- 			$tambah = mysqli_query($conn, "INSERT INTO tabe_kelas(id,username,password,level,fullname)VALUES('$id','$username','$password','$level','$fullname')");
- 		if ($tambah) {
+ 			$update = mysqli_query($conn, "UPDATE data SET id='$id',username='$username',password='$password',level='$level',fullname='$fullname' WHERE id='$id'");
+ 		if ($update) {
  			?>
              <script type="text/javascript">
- 				alert('Tamabah Data Berhasil :") ');
+ 				alert('Edit Data Berhasil');
  				document.location.href="index.php";
  			</script>
  			<?php
  		}
  		else{
- 			echo "yahh gagal:( ";
+ 			echo "yahh gagal";
  		}
  		}
  	 ?>
